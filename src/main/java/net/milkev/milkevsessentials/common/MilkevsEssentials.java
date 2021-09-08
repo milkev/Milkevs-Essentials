9f817b01dd70b27fe7e97bf379a2cfc6898692f8
milkev <kmdre@hotmail.com>
2021-09-07 21:08:57 -0600

    Extendo Grips and Flight Ring
    needs better textures, ring might be changed to flight glyph or something else because flight ring/angel ring is overused

--- src/main/java/net/milkev/milkevsessentials/common/MilkevsEssentials.java
+++ src/main/java/net/milkev/milkevsessentials/common/MilkevsEssentials.java
@@ -1,23 +1,34 @@
 package net.milkev.milkevsessentials.common;
 
+import dev.emi.trinkets.api.TrinketsApi;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.milkev.milkevsessentials.common.trinkets.ExtendoGrip;
+import net.milkev.milkevsessentials.common.trinkets.FlightRing;
+import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
+import org.lwjgl.system.CallbackI;
 
 public class MilkevsEssentials implements ModInitializer {
 
 
 	public static final String MOD_ID = "milkevsessentials";
 
-	public static final ExtendoGrip EXTENDO_GRIP = new ExtendoGrip(new FabricItemSettings(), 10, 10);
+	public static final ExtendoGrip EXTENDO_GRIP_LOW = new ExtendoGrip(new FabricItemSettings().maxCount(1).group(ItemGroup.TOOLS), 3, 0);
+	public static final ExtendoGrip EXTENDO_GRIP_NORMAL = new ExtendoGrip(new FabricItemSettings().maxCount(1).group(ItemGroup.TOOLS), 5, 5);
+	public static final ExtendoGrip EXTENDO_GRIP_HIGH = new ExtendoGrip(new FabricItemSettings().maxCount(1).group(ItemGroup.TOOLS), 15, 15);
 
+	public static final FlightRing FLIGHT_RING = new FlightRing(new FabricItemSettings().maxCount(1).group(ItemGroup.TOOLS));
 
 	@Override
 	public void onInitialize() {
 
-		Registry.register(Registry.ITEM, new Identifier(MOD_ID, "extendo_grip"), EXTENDO_GRIP);
+		Registry.register(Registry.ITEM, new Identifier(MOD_ID, "extendo_grip_low"), EXTENDO_GRIP_LOW);
+		Registry.register(Registry.ITEM, new Identifier(MOD_ID, "extendo_grip_normal"), EXTENDO_GRIP_NORMAL);
+		Registry.register(Registry.ITEM, new Identifier(MOD_ID, "extendo_grip_high"), EXTENDO_GRIP_HIGH);
+
+		Registry.register(Registry.ITEM, new Identifier(MOD_ID, "flight_ring"), FLIGHT_RING);
 
 		System.out.println(MOD_ID + " Initialized");
 	}
--- src/main/java/net/milkev/milkevsessentials/common/trinkets/ExtendoGrip.java
+++ src/main/java/net/milkev/milkevsessentials/common/trinkets/ExtendoGrip.java
@@ -2,23 +2,33 @@ package net.milkev.milkevsessentials.common.trinkets;
 
 import com.google.common.collect.Multimap;
 import com.jamieswhiteshirt.reachentityattributes.ReachEntityAttributes;
-import dev.emi.trinkets.api.SlotReference;
-import dev.emi.trinkets.api.TrinketItem;
+import dev.emi.trinkets.api.*;
+import net.milkev.milkevsessentials.common.MilkevsEssentials;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.attribute.EntityAttribute;
 import net.minecraft.entity.attribute.EntityAttributeModifier;
+import net.minecraft.entity.attribute.EntityAttributes;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
-
 import java.util.UUID;
 
 public class ExtendoGrip extends TrinketItem {
     private int reach;
     private int attack_reach;
+    //private int durability;
+    //private int maxDurability;
     public ExtendoGrip(Settings settings, int reach_set, int attack_reach_set) {
         super(settings);
+        TrinketsApi.registerTrinket(this, this);
         reach = reach_set;
         attack_reach = attack_reach_set;
     }
+
+    @Override
+    public void tick(ItemStack stack, SlotReference slot, LivingEntity entity) {
+        super.tick(stack, slot, entity);
+    }
+
     public Multimap<EntityAttribute, EntityAttributeModifier>
             getModifiers(ItemStack stack, SlotReference slot, LivingEntity entity, UUID uuid) {
 

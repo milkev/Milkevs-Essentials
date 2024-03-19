ad6318589e347bb772033977d99d093c22311725
Milkev <kmdre@hotmail.com>
2024-03-18 19:46:31 -0600

    Enable Meal API Support for 1.20.1
    Note that Meal API itself is broken when used with Sinytra, and thus this small compatability will not work when using Sinytra.

--- src/main/java/net/milkev/milkevsessentials/common/mixins/OPGluttonyCharmMixinMealAPI.java
+++ src/main/java/net/milkev/milkevsessentials/common/mixins/OPGluttonyCharmMixinMealAPI.java
@@ -2,7 +2,7 @@ package net.milkev.milkevsessentials.common.mixins;
 
 import dev.emi.trinkets.api.TrinketComponent;
 import dev.emi.trinkets.api.TrinketsApi;
-//import io.github.foundationgames.mealapi.api.v0.PlayerFullnessUtil;
+import io.github.foundationgames.mealapi.api.v0.PlayerFullnessUtil;
 import net.milkev.milkevsessentials.common.MilkevsEssentials;
 import net.minecraft.entity.player.HungerManager;
 import net.minecraft.entity.player.PlayerEntity;
@@ -16,15 +16,12 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(HungerManager.class)
 public abstract class OPGluttonyCharmMixinMealAPI {
 
-
-    @Shadow public abstract void add(int i, float f);
-
     @Inject(at = @At("HEAD"), method = "update")
     public void update(PlayerEntity playerEntity, CallbackInfo ci) {
         TrinketComponent trinket = TrinketsApi.getTrinketComponent(playerEntity).get();
         if(trinket.isEquipped(MilkevsEssentials.OP_GLUTTONY_CHARM)) {
             if(playerEntity instanceof ServerPlayerEntity player) {
-                //PlayerFullnessUtil.instance().addFullness(player, 1);
+                PlayerFullnessUtil.instance().addFullness(player, 1);
             }
         }
     }

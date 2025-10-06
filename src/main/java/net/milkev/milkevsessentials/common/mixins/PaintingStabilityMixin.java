ca494306bed38620ec3cf10c3931ce63b782597d
Milkev <kmdre@hotmail.com>
2025-10-06 02:00:53 -0600

    Remove log spam

--- src/main/java/net/milkev/milkevsessentials/common/mixins/PaintingStabilityMixin.java
+++ src/main/java/net/milkev/milkevsessentials/common/mixins/PaintingStabilityMixin.java
@@ -13,23 +13,20 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(AbstractDecorationEntity.class)
 public abstract class PaintingStabilityMixin {
-
-
+    
     @Shadow protected abstract Box calculateBoundingBox(BlockPos blockPos, Direction direction);
 
     @Inject(at = @At("RETURN"), method = "canStayAttached", cancellable = true) 
     public void makeItStay(CallbackInfoReturnable<Boolean> cir) {
         AbstractDecorationEntity entity = (AbstractDecorationEntity) (Object) this;
-        if(entity.age > 10) {
-            if (!entity.getWorld().isSpaceEmpty(entity)) {
-                boolean bl = (BlockPos.stream(this.calculateBoundingBox(entity.getAttachedBlockPos(), entity.getFacing()).contract(0.01)).anyMatch((blockPos) -> {
-                    BlockState blockState = entity.getWorld().getBlockState(blockPos);
-                    //System.out.println("Painting canStayAttached checked [" + blockPos + "] and the result was: " + (blockState.isFullCube(null, blockPos) ? "Full Cube" : "Non-Full Cube"));
-                    return blockState.isFullCube(null, blockPos);
-                }));
-                //System.out.println("Painting canStayAttached recalculated to; " + !bl);
-                cir.setReturnValue(!bl);
-            }
+        if (!entity.getWorld().isSpaceEmpty(entity)) {
+            boolean bl = (BlockPos.stream(this.calculateBoundingBox(entity.getAttachedBlockPos(), entity.getFacing()).contract(0.01)).anyMatch((blockPos) -> {
+                BlockState blockState = entity.getWorld().getBlockState(blockPos);
+                //System.out.println("Painting canStayAttached checked [" + blockPos + "] and the result was: " + (blockState.isFullCube(null, blockPos) ? "Full Cube" : "Non-Full Cube"));
+                return blockState.isFullCube(null, blockPos);
+            }));
+            //System.out.println("Painting canStayAttached recalculated to; " + !bl);
+            cir.setReturnValue(!bl);
         }
     }
 }
